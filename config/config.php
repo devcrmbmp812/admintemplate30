b3e424f896fac0867e8451f87d88f56010b661b7
andbel <andbel2323@gmail.com>
2019-10-16 09:49:05 +0300

    update for session again

--- config/config.php
+++ config/config.php
@@ -7,8 +7,6 @@ define('BASE_URL', 'http://developer.zenplan.dk/');
 define('APP_FOLDER', 'simpleadmin');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
 
-
-
 require_once BASE_PATH . '/lib/MysqliDb/MysqliDb.php';
 require_once BASE_PATH . '/helpers/helpers.php';
 
@@ -29,3 +27,4 @@ define('DB_NAME', "zenplan_dk_db");
 function getDbInstance() {
     return new MysqliDb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 }
+?>

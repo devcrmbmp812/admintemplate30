1ebd145ed1fc670c708639ba9449dafa058d7178
andbel <andbel2323@gmail.com>
2019-10-16 14:42:22 +0300

    update config

--- config/config.php
+++ config/config.php
@@ -2,9 +2,9 @@
 //Note: This file should be included first in every php page.
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-define('BASE_PATH', dirname(dirname(__FILE__)));
-//define('BASE_URL', 'http://developer.zenplan.dk/');
-define('BASE_URL', 'http://dev.phptemplate30.com:8081/');
+//define('BASE_PATH', dirname(dirname(__FILE__)));
+define('BASE_URL', 'http://developer.zenplan.dk/');
+//define('BASE_URL', 'http://dev.phptemplate30.com:8081/');
 define('APP_FOLDER', 'simpleadmin');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
 
